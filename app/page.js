e29abefcf49a731cfde0dc0fe7fdacd33315752b
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2023-04-18 18:53:13 +0900

    d

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
-export default function Home() {
+import { connectDB } from '@/util/database';
+//import { MongoClient } from 'mongodb';
+export default async function Home() {
+  const db = (await connectDB).db('forum');
+  let result = await db.collection('post').find().toArray();
+  console.log(result);
   return <div></div>;
 }
