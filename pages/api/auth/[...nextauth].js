ffe2c12e9f90ff9489eafebea46329ba1b6f6559
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2023-06-05 21:48:31 +0900

    d

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,20 @@
+import { connectDB } from '@/util/database';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-
 export default NextAuth({
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: {},
-        password: {},
+      id: 'forum-login',
+      name: 'Forum Account',
+      async authorize(credentials, res) {
+        const db = (await connectDB).db('forum');
+        const user = await db.collection('user').findOne({ id: credentials.id, password: credentials.password });
+        return user;
       },
-      async authorize(credentials, req) {},
+      credentials: {
+        id: { label: '아이디', type: 'text ' },
+        password: { label: '비밀번호', type: 'password' },
+      },
     }),
   ],
 });
